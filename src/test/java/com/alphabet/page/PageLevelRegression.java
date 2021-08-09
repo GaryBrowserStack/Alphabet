13d483f78df6e2b33353cc415f99a2843baa6e0d
gary_behan <gary.behan@nortonlifelock.com>
2021-08-09 11:46:00 +0100

    Simple use cases

--- src/test/java/com/alphabet/page/PageLevelRegression.java
+++ src/test/java/com/alphabet/page/PageLevelRegression.java
@@ -2,6 +2,7 @@ package com.alphabet.page;
 
 import com.alphabet.common.CommonLibrary;
 import com.paypal.selion.platform.asserts.SeLionAsserts;
+import com.paypal.selion.platform.html.support.HtmlElementUtils;
 import constants.generalConstants;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -25,63 +26,9 @@ public class PageLevelRegression extends CommonLibrary {
         driver.manage().deleteAllCookies();
         driver.get(testURL);
 
-        // Get the list of elements for each of the filter triggers.
-        List<WebElement> elementList = driver.findElements(By.xpath(generalConstants.productsFilter));
+        List<WebElement> productList = HtmlElementUtils.locateElements(generalConstants.productNamesXpath);
 
-        // Firstly we will check all of the apple products by filtering for Apple.
-        elementList.get(0).click();
-
-        // Call the function to check the information for all products in the current filter.
-        checkCartProductInformation();
+        SeLionAsserts.verifyFalse(productList.isEmpty());
     }
-
-    @Test(priority = 3)
-    public void testSamsungProducts() throws Exception {
-        // Before we load the page again, we should clear the cookies to ensure a fresh session
-        driver.manage().deleteAllCookies();
-        driver.get(testURL);
-
-        // Get the list of elements for each of the filter triggers.
-        List<WebElement> elementList = driver.findElements(By.xpath(generalConstants.productsFilter));
-
-        // Firstly we will check all of the apple products by filtering for Apple.
-        elementList.get(1).click();
-
-        // Call the function to check the information for all products in the current filter.
-        checkCartProductInformation();
-    }
-
-    @Test(priority = 4)
-    public void testGoogleProducts() throws Exception {
-        // Before we load the page again, we should clear the cookies to ensure a fresh session
-        driver.manage().deleteAllCookies();
-        driver.get(testURL);
-
-        // Get the list of elements for each of the filter triggers.
-        List<WebElement> elementList = driver.findElements(By.xpath(generalConstants.productsFilter));
-
-        // Firstly we will check all of the apple products by filtering for Apple.
-        elementList.get(2).click();
-
-        // Call the function to check the information for all products in the current filter.
-        checkCartProductInformation();
-    }
-
-    @Test(priority = 5)
-    public void testOnePlusProducts() throws Exception {
-        // Before we load the page again, we should clear the cookies to ensure a fresh session
-        driver.manage().deleteAllCookies();
-        driver.get(testURL);
-
-        // Get the list of elements for each of the filter triggers.
-        List<WebElement> elementList = driver.findElements(By.xpath(generalConstants.productsFilter));
-
-        // Firstly we will check all of the apple products by filtering for Apple.
-        elementList.get(2).click();
-
-        // Call the function to check the information for all products in the current filter.
-        checkCartProductInformation();
-    }
-
 
 }
